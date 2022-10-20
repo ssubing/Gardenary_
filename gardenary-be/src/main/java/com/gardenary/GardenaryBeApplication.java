4527a3bd91f8ad330e237dfdde344aee7a008799
SSAFY <naryung92@naver.com>
2022-10-20 12:43:57 +0900

    [build] 백엔드 초기 설정
    - application-db.properties 파일 gitignore

---
+++ gardenary-be/src/main/java/com/gardenary/GardenaryBeApplication.java
@@ -0,0 +1,13 @@
+package com.gardenary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenaryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenaryBeApplication.class, args);
+	}
+
+}
