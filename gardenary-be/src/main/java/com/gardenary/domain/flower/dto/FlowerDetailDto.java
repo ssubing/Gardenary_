62f4c7f4cf095ad0e9908b5bd8a63c92ba298c2b
신성은 <ibsun1215@gmail.com>
2022-10-25 16:09:57 +0900

    [fix] Dto 관련 오류 해결
    - Dto 안에 내용이 없어서 AllArgsConstructor와 NoArgsConstructor가 같아서 오류 발생, 내부에 변수 추가

--- gardenary-be/src/main/java/com/gardenary/domain/flower/dto/FlowerDetailDto.java
+++ gardenary-be/src/main/java/com/gardenary/domain/flower/dto/FlowerDetailDto.java
@@ -10,4 +10,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class FlowerDetailDto {
+    private int id;
 }
--- gardenary-be/src/main/java/com/gardenary/domain/flower/dto/QuestionAnswerDetailListDto.java
+++ gardenary-be/src/main/java/com/gardenary/domain/flower/dto/QuestionAnswerDetailListDto.java
@@ -10,4 +10,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class QuestionAnswerDetailListDto {
+    private int id;
 }
