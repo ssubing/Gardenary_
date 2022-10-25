beccb448b474ceedb65eda731c9c07a2a9183707
SSAFY <naryung92@naver.com>
2022-10-25 16:33:09 +0900

    [init] 회원 관련 기본 형식 파일 생성

---
+++ gardenary-be/src/main/java/com/gardenary/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gardenary.domain.user.repository;
+
+import com.gardenary.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
