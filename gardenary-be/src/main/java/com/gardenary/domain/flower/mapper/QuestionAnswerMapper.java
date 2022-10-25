271dd7ff914651214eed794b2cd2a57f34c31d9a
신성은 <ibsun1215@gmail.com>
2022-10-25 15:42:23 +0900

    [init] Question 관련 기본 형식 파일 생성
    - Entity(Question, QuestionAnswer) 작성
    - Dto, Mapper, Repository 파일 생성

---
+++ gardenary-be/src/main/java/com/gardenary/domain/flower/mapper/QuestionAnswerMapper.java
@@ -0,0 +1,9 @@
+package com.gardenary.domain.flower.mapper;
+
+import com.gardenary.domain.flower.dto.QuestionAnswerDto;
+import com.gardenary.domain.flower.entity.QuestionAnswer;
+import com.gardenary.global.common.mapper.EntityMapper;
+
+public interface QuestionAnswerMapper extends EntityMapper<QuestionAnswerDto, QuestionAnswer> {
+//    QuestionAnswerMapper mapper = Mappers.getMapper(QuestionAnswerMapper.class);
+}
