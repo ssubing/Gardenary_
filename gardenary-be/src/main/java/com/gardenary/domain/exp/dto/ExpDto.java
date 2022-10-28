7c219082a94b04328efea6b501d3664a040d2821
신성은 <ibsun1215@gmail.com>
2022-10-28 17:25:56 +0900

    [feat] Exp Dto, Mapper 작성
    - 경험치 관련 Dto 작성
    - 경험지 Mapper 작성

--- gardenary-be/src/main/java/com/gardenary/domain/exp/dto/ExpDto.java
+++ gardenary-be/src/main/java/com/gardenary/domain/exp/dto/ExpDto.java
@@ -5,10 +5,19 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ExpDto {
     private int id;
+    private UUID userId;
+    private LocalDateTime createdAt;
+    private int diaryId;
+    private boolean type;
+    private int expAmount;
+
 }
--- gardenary-be/src/main/java/com/gardenary/domain/exp/mapper/ExpMapper.java
+++ gardenary-be/src/main/java/com/gardenary/domain/exp/mapper/ExpMapper.java
@@ -3,11 +3,20 @@ package com.gardenary.domain.exp.mapper;
 import com.gardenary.domain.exp.dto.ExpDto;
 import com.gardenary.domain.exp.entity.Exp;
 import com.gardenary.global.common.mapper.EntityMapper;
-//import org.mapstruct.Mapper;
-//import org.mapstruct.Mapping;
-//import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface ExpMapper extends EntityMapper<ExpDto, Exp> {
 
-//    ExpMapper mapper = Mapper.getMapper(ExpMapper.class);
+    ExpMapper mapper = Mappers.getMapper(ExpMapper.class);
+
+    @Override
+    @Mapping(source = "user.id", target = "userId")
+    ExpDto toDto(final Exp entity);
+
+    @Override
+    @Mapping(source = "userId", target = "user.id")
+    Exp toEntity(final ExpDto dto);
 }
