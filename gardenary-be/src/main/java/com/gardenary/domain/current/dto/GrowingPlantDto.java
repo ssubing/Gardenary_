76f2a5b59c14e30ffe921d752a8e482af5afb74d
신성은 <ibsun1215@gmail.com>
2022-10-31 10:00:44 +0900

    [feat] 현재 식물 Dto, Mapper 작성
    - 현재 식물 Dto 작성
    - 현재 식물 Mapper 작성

--- gardenary-be/src/main/java/com/gardenary/domain/current/dto/GrowingPlantDto.java
+++ gardenary-be/src/main/java/com/gardenary/domain/current/dto/GrowingPlantDto.java
@@ -1,4 +1,21 @@
 package com.gardenary.domain.current.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GrowingPlantDto {
+    private int id;
+    private int myFlowerId;
+    private int myTreeId;
+    private UUID userId;
+    private int answerDays;
+    private int diaryDays;
 }
---
+++ gardenary-be/src/main/java/com/gardenary/domain/current/mapper/GrowingPlantMapper.java
@@ -0,0 +1,25 @@
+package com.gardenary.domain.current.mapper;
+
+import com.gardenary.domain.current.dto.GrowingPlantDto;
+import com.gardenary.domain.current.entity.GrowingPlant;
+import com.gardenary.global.common.mapper.EntityMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface GrowingPlantMapper extends EntityMapper<GrowingPlantDto, GrowingPlant> {
+    GrowingPlantMapper mapper = Mappers.getMapper(GrowingPlantMapper.class);
+
+    @Override
+    @Mapping(source = "myTree.id", target = "myTreeId")
+    @Mapping(source = "myFlower.id", target = "myFlowerId")
+    @Mapping(source = "user.id", target = "userId")
+    GrowingPlantDto toDto(final GrowingPlant entity);
+
+    @Override
+    @Mapping(source = "myTreeId", target = "myTree.id")
+    @Mapping(source = "myFlowerId", target = "myFlower.id")
+    @Mapping(source = "userId", target = "user.id")
+    GrowingPlant toEntity (final GrowingPlantDto dto);
+}
