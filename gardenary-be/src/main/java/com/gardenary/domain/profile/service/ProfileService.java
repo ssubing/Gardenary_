0faf13be02590526b8a4702d8473b02d92b9d586
SSAFY <naryung92@naver.com>
2022-11-03 13:39:36 +0900

    [feat] 프로필 조회 기능 구현 (S07P31A604-70)
    - 현재 캐릭터 조회
    - 닉네임 조회
    - 캐릭터 조회 api 경로 수정

---
+++ gardenary-be/src/main/java/com/gardenary/domain/profile/service/ProfileService.java
@@ -0,0 +1,15 @@
+package com.gardenary.domain.profile.service;
+
+import com.gardenary.domain.avatar.dto.response.AvatarResponseDto;
+import com.gardenary.domain.profile.dto.response.ProfileResponseDto;
+import com.gardenary.domain.user.entity.User;
+
+import java.util.List;
+
+public interface ProfileService {
+
+    ProfileResponseDto getProfile(User user);
+
+    List<AvatarResponseDto> getAvatar(User user);
+
+}
