b1932828f984075ca7c6453ced44cfe388f34851
subin <5515983@naver.com>
2022-11-04 17:39:00 +0900

    [feat] 텃밭 조회 API 요청 - 성공적으로 요청을 하고 json 데이터를 받아옴 - status와 message는 정상적 - responseDto를 제대로 받는 방법은 아직인 상태

---
+++ FEGardenary/Assets/5Scripts/Main/Table/TreeBookDto.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeBookDto
+{
+    public string name { get; set; }
+    public string content { get; set; }
+    public string assetId { get; set; }
+    public bool acquired { get; set; }
+}
