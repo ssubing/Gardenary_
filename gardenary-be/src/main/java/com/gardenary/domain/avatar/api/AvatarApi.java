ad98a5d0c78d5c39933431750c708b5c2a34e0a4
신성은 <ibsun1215@gmail.com>
2022-11-07 17:14:10 +0900

    [feat] 아바타 획득 기능 구현 (S07P31A604-212)
    - 관련 기능 api 추가
    - 결과 반환 Dto 추가
    - Service와 ServiceImpl에 각 조건 함수 설정 및 조건 만족시 DB에 저장하고 ResponseDto에 추가하는 함수 추가

---
+++ gardenary-be/src/main/java/com/gardenary/domain/avatar/api/AvatarApi.java
@@ -0,0 +1,35 @@
+package com.gardenary.domain.avatar.api;
+
+import com.gardenary.domain.avatar.dto.response.AvatarListResponseDto;
+import com.gardenary.domain.avatar.dto.response.AvatarResponseDto;
+import com.gardenary.domain.avatar.service.AvatarService;
+import com.gardenary.global.common.response.DtoResponse;
+import com.gardenary.global.config.security.UserDetail;
+import com.gardenary.global.properties.ResponseProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/avatar")
+@RequiredArgsConstructor
+public class AvatarApi {
+
+    private final ResponseProperties responseProperties;
+    private final AvatarService avatarServcie;
+
+    @PostMapping("/{type}")
+    public ResponseEntity<DtoResponse<AvatarListResponseDto>> getNewAvatar(@AuthenticationPrincipal UserDetail userDetail, @PathVariable int type) {
+        AvatarListResponseDto result = avatarServcie.getNewAvatar(userDetail.getUser(), type);
+        if(result == null) {
+            return ResponseEntity.status(HttpStatus.OK).body(DtoResponse.of(HttpStatus.OK, responseProperties.getFail(), null));
+        } else{
+            return ResponseEntity.status(HttpStatus.OK).body(DtoResponse.of(HttpStatus.OK, responseProperties.getSuccess(), result));
+        }
+    }
+}
