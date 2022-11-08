f832ae8805f773a6380ab89657435ebef97764e8
신성은 <ibsun1215@gmail.com>
2022-11-08 10:50:32 +0900

    [fix] 랜덤 꽃 생성시에 id를 찾기 위한 인덱스 배열 추가 (S07P31A604-231)
    - flowerIdx 배열 추가
    - 매일 새벽 3시 질문 랜덤 선택하는 방식 수정
    - FlowerRepository에 id로 해당 꽃 찾는 JPA 추가

--- gardenary-be/src/main/java/com/gardenary/domain/flower/repository/FlowerRepository.java
+++ gardenary-be/src/main/java/com/gardenary/domain/flower/repository/FlowerRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface FlowerRepository extends JpaRepository<Flower, Integer> {
     List<Flower> findAllByOrderByNameAscIdAsc();
+    Flower findById(String flowerIdx);
 }
