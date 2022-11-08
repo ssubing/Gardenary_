5772e9b9023a6ad82d2c9ef8b31f6d9f5e514756
신성은 <ibsun1215@gmail.com>
2022-11-08 10:52:20 +0900

    [fix] 랜덤 꽃 생성시에 id를 찾기 위한 인덱스 배열 추가 (S07P31A604-251)
    - api에 오타 수정
    - serviceImpl에서 flower리스트를 받지 않고 아바타 받는 방식으로 수정

--- gardenary-be/src/main/java/com/gardenary/domain/avatar/service/AvatarServiceImpl.java
+++ gardenary-be/src/main/java/com/gardenary/domain/avatar/service/AvatarServiceImpl.java
@@ -6,20 +6,18 @@ import com.gardenary.domain.avatar.entity.Avatar;
 import com.gardenary.domain.avatar.entity.MyAvatar;
 import com.gardenary.domain.avatar.repository.AvatarRepository;
 import com.gardenary.domain.avatar.repository.MyAvatarRepository;
-import com.gardenary.domain.flower.entity.Flower;
 import com.gardenary.domain.flower.entity.MyFlower;
 import com.gardenary.domain.flower.repository.FlowerRepository;
 import com.gardenary.domain.flower.repository.MyFlowerRepository;
 import com.gardenary.domain.tree.entity.MyTree;
-import com.gardenary.domain.tree.entity.Tree;
 import com.gardenary.domain.tree.repository.MyTreeRepository;
 import com.gardenary.domain.tree.repository.TreeRepository;
 import com.gardenary.domain.user.entity.User;
+import com.gardenary.global.properties.ConstProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -37,6 +35,8 @@ public class AvatarServiceImpl implements AvatarService {
     private final TreeRepository treeRepository;
     private final MyFlowerRepository myFlowerRepository;
     private final FlowerRepository flowerRepository;
+    private final ConstProperties constProperties;
+    private static final String[] flowerIdx = {"0_0", "0_1", "0_2", "1_0", "1_1", "1_2", "2_0", "2_1", "2_2", "3_0", "3_1", "4_0", "5_0", "6_0", "7_0", "8_0", "9_0", "10_0", "10_1", "11_0", "12_0", "13_0", "14_0", "14_1", "14_2", "14_3", "14_4", "15_0", "16_0", "16_1", "17_0", "18_0", "19_0", "19_1", "20_0", "20_1", "20_2", "20_3", "21_0", "21_1", "21_2", "21_3", "21_4", "21_5", "22_0", "23_0", "23_1", "23_2", "24_0", "25_0", "25_1", "25_2", "25_3", "26_0", "26_1", "26_2", "26_3", "26_4", "26_5", "27_0"};
 
     @Override
     public AvatarListResponseDto getNewAvatar(User user, int type) {
@@ -52,9 +52,8 @@ public class AvatarServiceImpl implements AvatarService {
         //꽃일 경우
         if(type == 1) {
             List<MyFlower> myFlowerList = myFlowerRepository.findAllByUser(user);
-            List<Flower> flowerList = flowerRepository.findAll();
             myFlowerList.remove(myFlowerList.size()-1);
-            if(!haveAvatar.contains(2) && get2Avatar(myFlowerList, flowerList)) {
+            if(!haveAvatar.contains(2) && get2Avatar(myFlowerList)) {
                 listAddAndSave(1, avatarResponseDtoList, avatarList, user);
             }
             if(!haveAvatar.contains(5) && get5Avatar(myFlowerList)) {
@@ -62,16 +61,15 @@ public class AvatarServiceImpl implements AvatarService {
             }
             if(!haveAvatar.contains(6)) {
                 List<MyTree> myTreeList = myTreeRepository.findAllByUser(user);
-                List<Tree> treeList = treeRepository.findAll();
                 myTreeList.remove(myTreeList.size() - 1);
-                if(get6Avatar(myFlowerList, flowerList, myTreeList, treeList)){
+                if(get6Avatar(myFlowerList, myTreeList)){
                     listAddAndSave(5, avatarResponseDtoList, avatarList, user);
                 }
             }
             if(!haveAvatar.contains(7) && get7Avatar(myFlowerList)) {
                 listAddAndSave(6, avatarResponseDtoList, avatarList, user);
             }
-            if(!haveAvatar.contains(8) && get8Avatar(myFlowerList, flowerList)) {
+            if(!haveAvatar.contains(8) && get8Avatar(myFlowerList)) {
                 listAddAndSave(7, avatarResponseDtoList, avatarList, user);
             }
         }
@@ -79,9 +77,8 @@ public class AvatarServiceImpl implements AvatarService {
         //나무일 경우
         if(type == 2) {
             List<MyTree> myTreeList = myTreeRepository.findAllByUser(user);
-            List<Tree> treeList = treeRepository.findAll();
             myTreeList.remove(myTreeList.size() - 1);
-            if(!haveAvatar.contains(1) && get1Avatar(myTreeList, treeList)) {
+            if(!haveAvatar.contains(1) && get1Avatar(myTreeList)) {
                 listAddAndSave(0, avatarResponseDtoList, avatarList, user);
             }
             if(!haveAvatar.contains(4) && get4Avatar(myTreeList)) {
@@ -89,16 +86,15 @@ public class AvatarServiceImpl implements AvatarService {
             }
             if(!haveAvatar.contains(6)) {
                 List<MyFlower> myFlowerList = myFlowerRepository.findAllByUser(user);
-                List<Flower> flowerList = flowerRepository.findAll();
                 myFlowerList.remove(myFlowerList.size()-1);
-                if(get6Avatar(myFlowerList, flowerList, myTreeList, treeList)){
+                if(get6Avatar(myFlowerList, myTreeList)){
                     listAddAndSave(5, avatarResponseDtoList, avatarList, user);
                 }
             }
             if(!haveAvatar.contains(9) && get9Avatar(myTreeList)) {
                 listAddAndSave(8, avatarResponseDtoList, avatarList, user);
             }
-            if(!haveAvatar.contains(10) && get10Avatar(myTreeList, treeList)) {
+            if(!haveAvatar.contains(10) && get10Avatar(myTreeList)) {
                 listAddAndSave(9, avatarResponseDtoList, avatarList, user);
             }
         }
@@ -126,14 +122,15 @@ public class AvatarServiceImpl implements AvatarService {
     }
 
     //같은 종류 나무 3가지
-    public boolean get1Avatar(List<MyTree> myTreeList, List<Tree> treeList) {
-        HashMap<String, Integer> treeMap = new HashMap<>();
-        for (Tree tree : treeList) {
-            treeMap.put(tree.getName(), 0);
-        }
+    public boolean get1Avatar(List<MyTree> myTreeList) {
+        HashMap<String, Integer> myTreeMap = new HashMap<>();
         for(MyTree myTree : myTreeList) {
-            treeMap.put(myTree.getTree().getName(), treeMap.get(myTree.getTree().getName()) + 1);
-            if(treeMap.get(myTree.getTree().getName()) == 3) {
+            if(myTreeMap.containsKey(myTree.getTree().getName())) {
+                myTreeMap.put(myTree.getTree().getName(), myTreeMap.get(myTree.getTree().getName()) + 1);
+            } else {
+                myTreeMap.put(myTree.getTree().getName(), 1);
+            }
+            if(myTreeMap.get(myTree.getTree().getName()) == 3) {
                 return true;
             }
         }
@@ -141,32 +138,27 @@ public class AvatarServiceImpl implements AvatarService {
     }
 
     //모든 꽃
-    public boolean get2Avatar(List<MyFlower> myFlowerList, List<Flower> flowerList) {
+    public boolean get2Avatar(List<MyFlower> myFlowerList) {
         HashSet<String> myFlowerSet = new HashSet<>();
         for(MyFlower myFlower : myFlowerList) {
             myFlowerSet.add(myFlower.getFlower().getId());
         }
-        for (Flower flower : flowerList) {
-            if (!myFlowerSet.contains(flower.getId())) {
-                return false;
-            }
-        }
-        return true;
+        return myFlowerSet.size() == constProperties.getFlowerSize();
     }
 
     //아무 나무 하나 성장 완료
     public boolean get4Avatar(List<MyTree> myTreeList) {
-        return myTreeList.size() == 2;
+        return myTreeList.size() == 1;
     }
 
     //아무 꽃 하나 성장 완료
     public boolean get5Avatar(List<MyFlower> myFlowerList) {
-        return myFlowerList.size() == 2;
+        return myFlowerList.size() == 1;
     }
 
     //모든 꽃과 나무
-    public boolean get6Avatar(List<MyFlower> myFlowerList, List<Flower> flowerList, List<MyTree> myTreeList, List<Tree> treeList) {
-        return get2Avatar(myFlowerList, flowerList) && get10Avatar(myTreeList, treeList);
+    public boolean get6Avatar(List<MyFlower> myFlowerList, List<MyTree> myTreeList) {
+        return get2Avatar(myFlowerList) && get10Avatar(myTreeList);
     }
 
     //서로 다른 꽃 5개 수집
@@ -182,16 +174,15 @@ public class AvatarServiceImpl implements AvatarService {
     }
 
     //같은 꽃 3개
-    public boolean get8Avatar(List<MyFlower> myFlowerList, List<Flower> flowerList) {
-        HashMap<String, Integer> flowerMap = new HashMap<>();
-        for (Flower flower : flowerList) {
-            if(!flowerMap.containsKey(flower.getName())){
-                flowerMap.put(flower.getName(), 0);
-            }
-        }
+    public boolean get8Avatar(List<MyFlower> myFlowerList) {
+        HashMap<String, Integer> myFlowerMap = new HashMap<>();
         for(MyFlower myFlower : myFlowerList) {
-            flowerMap.put(myFlower.getFlower().getName(), flowerMap.get(myFlower.getFlower().getName()) + 1);
-            if(flowerMap.get(myFlower.getFlower().getName()) == 3) {
+            if(myFlowerMap.containsKey(myFlower.getFlower().getName())) {
+                myFlowerMap.put(myFlower.getFlower().getName(), myFlowerMap.get(myFlower.getFlower().getName()) + 1);
+            } else {
+                myFlowerMap.put(myFlower.getFlower().getName(), 1);
+            }
+            if(myFlowerMap.get(myFlower.getFlower().getName()) == 3) {
                 return true;
             }
         }
@@ -211,16 +202,11 @@ public class AvatarServiceImpl implements AvatarService {
     }
 
     //모든 나무 수집
-    public boolean get10Avatar(List<MyTree> myTreeList, List<Tree> treeList) {
+    public boolean get10Avatar(List<MyTree> myTreeList) {
         HashSet<String> myTreeSet = new HashSet<>();
         for(MyTree myTree : myTreeList) {
             myTreeSet.add(myTree.getTree().getName());
         }
-        for (Tree tree : treeList) {
-            if(!myTreeSet.contains(tree.getName())) {
-                return false;
-            }
-        }
-        return true;
+        return myTreeSet.size() == constProperties.getTreeSize();
     }
 }
